5035a42c4d559a33494e2c3b96272152a8a571dc
Enes Can <enescx54@gmail.com>
2026-09-06 18:36:27 +0300

    fix(api): set trust proxy in express app

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,8 @@ import { i18nMiddleware } from "@/middlewares/i18n.middleware";
 
 const app = express();
 
+app.set("trust proxy", 1);
+
 app.use(cors());
 app.use(express.json());
 app.use(i18nMiddleware);
